fc1c27ff6f2ee3036c72ec096736b55d2389a300
jonjaeg <joni.jaeger1@gmail.com>
2025-10-24 11:53:37 +0200

    add feature: hamming_distance in ff_structure crate

--- crates/ff_structure/src/lib.rs
+++ crates/ff_structure/src/lib.rs
@@ -1,12 +1,17 @@
+/// The crate ff_structure contains submodules error, dotbracket, pair_table, multi_pair_table, loop_table and hamming distance.
+/// and makes them globally usable.
+
 mod error;
 mod dotbracket;
 mod pair_table;
 mod multi_pair_table;
 mod loop_table;
+mod hamming_distance;
 
 pub use error::*;
 pub use dotbracket::*;
 pub use pair_table::*;
 pub use multi_pair_table::*;
 pub use loop_table::*;
+pub use hamming_distance::*;
 
